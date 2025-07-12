639c81a02b005bab691e001c6387480c094b02f3
Lindow-Shiba <rapidtuber12@gmail.com>
2025-07-12 17:40:12 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 
 import './globals.css';
-
 export const metadata = { title:"Raven's Scrap & Supply" };
-
-export default function RootLayout({children}){
+export default function RootLayout({children}) {
   return <html lang="en"><body>{children}</body></html>;
 }
