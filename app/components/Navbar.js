b08fa6c152d01b6e98467d311b528d0a8da2b817
Lindow-Shiba <rapidtuber12@gmail.com>
2025-07-13 09:49:42 +0100

    Create Navbar.js

---
+++ app/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', background: '#00b0f4', color: 'white' }}>
+      <h1>Raven Scrap and Supply</h1>
+    </nav>
+  );
+}
