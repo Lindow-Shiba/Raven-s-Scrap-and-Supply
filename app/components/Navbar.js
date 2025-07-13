336845f7d2faed98dbbabf6498f8563e347d3ac5
Lindow-Shiba <rapidtuber12@gmail.com>
2025-07-13 10:09:38 +0100

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,12 +2,12 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import ravenLogo from '../public/raven-logo.png';
 
 export default function Navbar() {
   return (
     <nav style={{ backgroundColor: '#000', color: '#FFD700', padding: '10px 20px', display: 'flex', alignItems: 'center' }}>
-      <Image src={ravenLogo} alt="Raven Logo" width={50} height={50} />
+      {/* Use the image from the root public folder */}
+      <Image src="/raven-logo.png" alt="Raven Logo" width={50} height={50} />
       <h1 style={{ marginLeft: '15px', flexGrow: 1 }}>Raven Scrap and Supply</h1>
       <div>
         <Link href="/"><a style={{ color: '#FFD700', marginRight: '20px' }}>Home</a></Link>
