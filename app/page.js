5cae2dd05fe5ab4ee0ea1263793d7a30c71a16c0
Lindow-Shiba <rapidtuber12@gmail.com>
2025-07-31 16:28:00 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -137,7 +137,7 @@ export default function Home() {
               </select>
               <select value={wh} onChange={e => setWh(e.target.value)} style={{ padding: 8 }}>
                 <option value="">Select Warehouse</option>
-                <option>Benny&apos;s</option><option>Michael&Norma</option>
+                <option>Michaelthepotatofarmer&apos;s</option><option>Michael&Norma</option>
               </select>
               <input value={inv} onChange={e => setInv(e.target.value)} style={{ padding: 8 }} />
               <input placeholder="Notes" value={notes} onChange={e => setNotes(e.target.value)} style={{ padding: 8 }} />
